0cc1de331f7d8f7aecdd96c2b6237bbc247cafb6
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2023-07-27 14:21:18 +0700

    update

--- run_multiclient.sh
+++ run_multiclient.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Replace SERVER_ADDRESS and SERVER_PORT with your server's address and port
-SERVER_PORT="9999"
+SERVER_PORT="8888"
 
-NUM_CLIENTS=2
+NUM_CLIENTS=100
 
 # Function to launch a client in the background
 launch_client() {
-    ./build/multiclient "$SERVER_PORT" &
+    ./build/clone-client "$SERVER_PORT" &
 }
 
 # Loop to create and run 1000 clients
 for ((i=1; i<=NUM_CLIENTS; i++))
 do
     launch_client
-    sleep 0.5  # Adjust this sleep time as needed to avoid overwhelming the system
+    # sleep 0.1  # Adjust this sleep time as needed to avoid overwhelming the system
 done
 
 # Wait for all background clients to finish
