764a7c6fcb96081270dbdb936834571a7b36bad5
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2023-07-27 14:34:53 +0700

    update

--- Client/clone-client.c
+++ Client/clone-client.c
@@ -22,6 +22,7 @@ void *send_message(void *client_sockfd)
 {
     int socket = *(int *)client_sockfd;
     char client_name[1024];
+    srand(time(NULL));
     int random_num = rand() % 1000;
 
     // enter name and check then send to server
