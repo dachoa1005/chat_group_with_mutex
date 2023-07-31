776b8fafb0a46fc0998876db1cbbc09ea9333736
dachoa1005 <hoa.pd185357@sis.hust.edu.vn>
2023-07-31 23:41:49 +0700

    update

--- run_multiclient.sh
+++ run_multiclient.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace SERVER_ADDRESS and SERVER_PORT with your server's address and port
-SERVER_PORT="99999"
+SERVER_PORT="88888"
 
 NUM_CLIENTS=1000
 
@@ -14,7 +14,7 @@ launch_client() {
 for ((i=1; i<=NUM_CLIENTS; i++))
 do
     launch_client
-    # sleep 0.01  # Adjust this sleep time as needed to avoid overwhelming the system
+    sleep 0.1 # Adjust this sleep time as needed to avoid overwhelming the system
 done
 
 # Wait for all background clients to finish
